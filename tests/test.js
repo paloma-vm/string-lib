f230d2dc2f7df66e0957a8f5356f8fbfe0f5a9cc
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-08-28 11:27:38 -0600

    changed main and version no.

--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,14 @@
-const lib = require('../src/index.js')
+const lib = require('../src/index.js');
 
 test('Sanity check', () => {
-    expect(2+2).toBe(4)
-})
+  expect(2 + 2).toBe(4);
+});
 
 test('test capitalize', () => {
-    expect(lib.capitalize('hello')).toBe('Hello')
-})
+  expect(lib.capitalize('hello')).toBe('Hello');
+});
 
 test('test allCaps', () => {
-    expect(lib.allCaps('hello world')).toBe('HELLO WORLD')
-})
+  expect(lib.allCaps('hello world')).toBe('HELLO WORLD');
+});
 // remember to check coverage
