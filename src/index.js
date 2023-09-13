1e2f366cd1c1d12466f1189d0d8151a930820c80
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-09-12 22:37:29 -0700

    added more tests, cleanup

--- src/index.js
+++ src/index.js
@@ -1,58 +1,39 @@
-// CHALLENGE 1
 function capitalize(str) {
   const firstChar = str[0];
   const upperFirstChar = firstChar.toUpperCase();
   const otherChars = str.slice(1);
 
   const result = upperFirstChar + otherChars;
-  // console.log(result)
   return result;
 }
 
-capitalize('hello');
-
-// CHALLENGE 2
 function allCaps(str) {
   const result = str.toUpperCase();
-  console.log(result);
-
   return result;
 }
 
 allCaps('hello world');
 
-// CHALLENGE 3
 function capitalizeWords(str) {
   const splitString = str.split(' ');
-  // console.log(splitString)
   const allWordsCap = splitString.map(((word) => capitalize(word)));
   const result = allWordsCap.join(' ');
-  console.log(result);
   return result;
 }
 
 capitalizeWords('do all the things');
 
-// CHALLENGE 4
 function removeExtraSpaces(str) {
   const noSpaces = str.trim(); // removes white space from beginning and end of a string
-  // console.log(noSpaces)
   const noSpacesArray = noSpaces.split(' ');
-  // console.log(noSpacesArray)
   const filteredArray = noSpacesArray.filter((word) => word !== ''); // filters empty strings
-  // console.log(filteredArray)
   const result = filteredArray.join(' ');
-  console.log(result);
   return result;
 }
 
-removeExtraSpaces('  Hello   World   ');
-
-// CHALLENGE 5
 function kebabCase(str) { // remove extra spaces, replace spaces w/hyphen, all lowercase
   const permittedChars = [];
   const lowerStr = str.toLowerCase();
-  // console.log(lowerStr)
   const characters = lowerStr.split('');
   for (let i = 0; i < characters.length; i += 1) {
     const charCode = lowerStr.charCodeAt(i);
@@ -64,27 +45,19 @@ function kebabCase(str) { // remove extra spaces, replace spaces w/hyphen, all l
             || (charCode >= 97 && charCode <= 122) // lowercase letters
     ) {
       permittedChars.push(characters[i]);
-      // console.log(permittedChars)
     }
   }
 
   const newStr = permittedChars.join('');
-  // console.log(newStr)
   const newStrNoExtraSpaces = removeExtraSpaces(newStr);
-  // console.log(newStrNoExtraSpaces)
   const result = newStrNoExtraSpaces.split(' ').join('-');
-  console.log(result);
   return result;
 }
 
-kebabCase(' Hello  World  ');
-
-// CHALLENGE 6
 function snakeOrKebabCase(str, separatingChar) { // remove extra spaces,
   // replace spaces w/underscore or hyphen, all lowercase
   const permittedChars = [];
   const lowerStr = str.toLowerCase();
-  // console.log(lowerStr)
   const characters = lowerStr.split('');
   for (let i = 0; i < characters.length; i += 1) {
     const charCode = lowerStr.charCodeAt(i);
@@ -96,25 +69,17 @@ function snakeOrKebabCase(str, separatingChar) { // remove extra spaces,
             || (charCode >= 97 && charCode <= 122) // lowercase letters
     ) {
       permittedChars.push(characters[i]);
-      // console.log(permittedChars)
     }
   }
 
   const newStr = permittedChars.join('');
-  // console.log(newStr)
   const newStrNoExtraSpaces = removeExtraSpaces(newStr);
-  // console.log(newStrNoExtraSpaces)
   const result = newStrNoExtraSpaces.split(' ').join(separatingChar);
-  console.log(result);
   return result;
 }
 
-snakeOrKebabCase('  what the   heck  ', '_');
-
-// CHALLENGE 7
 function camelCase(str) {
   const words = str.split(' '); // to get an array of words
-  // console.log(splitString)
   const wordsToConvert = words.map((word, i) => {
     if (i === 0) {
       return word.toLowerCase();
@@ -122,21 +87,14 @@ function camelCase(str) {
     return capitalize(word);
   });
   const result = wordsToConvert.join(''); // join on an empty string
-  console.log(result);
   return result;
 }
 
-camelCase('Camel Case');
-
-// CHALLENGE 8
 function shift(str) { // take first char of str and move to end of str
   const firstChar = str[0];
   const remainingChars = str.slice(1); // removes first character and returns the
   // remainder of the string
-  // console.log(firstChar)
-  // console.log(remainingChars)
   const result = remainingChars + firstChar;
-  console.log(result);
   return result;
 }
 shift('Howdy Planet');
@@ -156,12 +114,8 @@ function makeHashTag(str) { // convert string to hash tag
     hashTags[i] = `#${threeLongestWords[i]}`;
   }
 
-  console.log(hashTags);
   return hashTags;
 }
-
-makeHashTag('Amazing bongo drums for sale');
-makeHashTag('Mediocre bongo drums for discount price');
 
 // CHALLENGE 10
 function isEmpty(str) { // returns True if empty or only whitespace (spaces, line returns, tabs)
@@ -181,15 +135,6 @@ function isEmpty(str) { // returns True if empty or only whitespace (spaces, lin
   return true; // must be outside the loop
 }
 
-const result = isEmpty('ABC def');
-console.log(result);
-const result2 = isEmpty('          ');
-console.log(result2);
-
-// CHALLENGE 11
-// add documentation
-
-// is there a way to export all at once?
 // export modules
 module.exports.capitalize = capitalize;
 module.exports.allCaps = allCaps;
--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,6 @@
-const lib = require('../src/index.js');
+/* eslint-env jest */ // this is for eslint to recognize the jest keywords
 
-test('Sanity check', () => {
-  expect(2 + 2).toBe(4);
-});
+const lib = require('../src/index');
 
 test('test capitalize', () => {
   expect(lib.capitalize('hello')).toBe('Hello');
@@ -11,4 +9,37 @@ test('test capitalize', () => {
 test('test allCaps', () => {
   expect(lib.allCaps('hello world')).toBe('HELLO WORLD');
 });
-// remember to check coverage
+
+test('test capitalizeWords', () => {
+  expect(lib.capitalizeWords('do all the things')).toBe('Do All The Things');
+});
+
+test('test removeExtraSpaces', () => {
+  expect(lib.removeExtraSpaces('  Hello   World   ')).toBe('Hello World');
+});
+
+test('test kebabCase', () => {
+  expect(lib.kebabCase('Hello World')).toBe('hello-world');
+});
+
+test('test snakeOrKebabCase', () => {
+  expect(lib.snakeOrKebabCase('Hello World', '_')).toBe('hello_world');
+  expect(lib.snakeOrKebabCase('  what the   heck  ', '_')).toBe('what_the_heck');
+});
+
+test('test camelCase', () => {
+  expect(lib.camelCase('Hello World')).toBe('helloWorld');
+});
+
+test('test shift', () => {
+  expect(lib.shift('Hello World')).toBe('ello WorldH');
+});
+
+test('test makeHashTag', () => {
+  expect(lib.makeHashTag('Amazing bongo drums for sale')).toStrictEqual(['#amazing', '#bongo', '#drums']);
+});
+
+test('test isEmpty', () => {
+  expect(lib.isEmpty('ABC def')).toBe(false);
+  expect(lib.isEmpty('          ')).toBe(true);
+});
